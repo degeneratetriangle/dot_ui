77c62aa1b9b887c5430e505dd5ccec5094569158
Stephen Wood <stephenwood@Stephens-MBP.hsd1.ca.comcast.net>
2017-08-03 08:16:09 -0700

    not working yet, but getting there

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Menu from './Menu';
+import Wizard from './Wizard';
 
 
 class App extends Component {
   render() {
     return (
         <MuiThemeProvider>
-            <Menu />
+            <div>
+                <Menu />
+                <Wizard />
+            </div>
         </MuiThemeProvider>
     );
   }
